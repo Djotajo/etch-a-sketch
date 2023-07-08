645125c6f359fb2e7bb24bf34fda4e38427a03d3
Djotajo <20823564+Djotajo@users.noreply.github.com>
2023-07-08 09:20:28 +0200

    Create menu, menu buttons, color picker, fix grid

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 const container = document.querySelector("#container")
 
+const colorElement = document.querySelector("#color");
+let color = colorElement.value;
+
+colorElement.addEventListener("input", () => {
+    color = colorElement.value;
+    console.log(color);
+})
+
+function altPaintSquare(e) {
+    e.style.backgroundColor = color;
+}
+
 for (let i = 0; i< 16; i++) {
     let newRow = document.createElement("div");
     for (let n = 0; n< 16; n++) {
         let newDiv = document.createElement("div");
-        newDiv.textContent = "+";
-        newDiv.classList.add("square")
+        newDiv.textContent = "";
+        newDiv.classList.add("gridSquare")
         newRow.appendChild(newDiv);
     }
     container.appendChild(newRow);
 }
+
+function paintSquare(e) {
+    e.classList.add("squarePaint");
+}
+
+const squares = document.querySelectorAll(".gridSquare");
+for (let square of squares) {
+    square.addEventListener("mouseover", function(e) {
+        console.log(this);
+        altPaintSquare(this);
+    });
+}
+
